3bf90801298cbf292032e97bc22158afc54c500d
Hariharan Kanakaraja <hariharankanakaraja@gmail.com>
2024-12-01 14:21:29 +0530

    fragment example

--- app/src/main/java/com/nibm/hari/helabojan/ui/home/HomeActivity.kt
+++ app/src/main/java/com/nibm/hari/helabojan/ui/home/HomeActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.nibm.hari.helabojan.R
 import com.nibm.hari.helabojan.databinding.ActivityHomeBinding
 import com.nibm.hari.helabojan.databinding.ActivitySignInBinding
@@ -19,5 +22,9 @@ class HomeActivity : Activity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navMenu
+        val navController = findNavController(R.id.nav_host_fragment_view)
+        navView.setupWithNavController(navController)
     }
 }
