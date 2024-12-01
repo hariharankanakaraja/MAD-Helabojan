409ff4e5e018935dcf822681563da911b4b363b0
Hariharan Kanakaraja <hariharankanakaraja@gmail.com>
2024-12-01 12:22:35 +0530

    firebase login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -61,4 +62,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-auth")
+
 }
--- app/src/main/java/com/nibm/hari/helabojan/ui/authentication/signin/SignInActivity.kt
+++ app/src/main/java/com/nibm/hari/helabojan/ui/authentication/signin/SignInActivity.kt
@@ -1,23 +1,63 @@
 package com.nibm.hari.helabojan.ui.authentication.signin
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.nibm.hari.helabojan.R
 import com.nibm.hari.helabojan.databinding.ActivitySignInBinding
 import com.nibm.hari.helabojan.databinding.ActivityWelcomeBinding
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
+import com.nibm.hari.helabojan.ui.home.HomeActivity
+
 
 class SignInActivity : Activity() {
 
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnSignIn.setOnClickListener{
+
+            val email = binding.usernameInp.text.toString().trim()
+            val password = binding.passwordInp.text.toString().trim()
+
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                //signin in with firebase email auth
+                signInWithEmail(email, password)
+            }
+
+        }
+
     }
+
+    fun signInWithEmail(email: String, password: String){
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    navigateHomeActivity()
+                } else {
+                }
+            }
+    }
+
+    fun navigateHomeActivity(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
 }
